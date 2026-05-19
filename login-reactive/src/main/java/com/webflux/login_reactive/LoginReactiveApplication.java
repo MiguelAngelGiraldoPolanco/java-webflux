52642cd1dceff7a427f564f15d6a5604f2293a71
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-19 15:18:36 +0200

    create: Mono repo for webflux

---
+++ login-reactive/src/main/java/com/webflux/login_reactive/LoginReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.login_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginReactiveApplication.class, args);
+	}
+
+}
---
+++ login-reactive/src/test/java/com/webflux/login_reactive/LoginReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.login_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
