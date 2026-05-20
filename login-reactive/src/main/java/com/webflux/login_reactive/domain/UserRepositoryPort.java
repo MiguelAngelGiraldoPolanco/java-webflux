1e00bf6211110c5bb75b518590212847c808e082
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-20 11:23:45 +0200

    fix: add delete and change save method

--- login-reactive/src/main/java/com/webflux/login_reactive/domain/UserRepositoryPort.java
+++ login-reactive/src/main/java/com/webflux/login_reactive/domain/UserRepositoryPort.java
@@ -4,5 +4,5 @@ import reactor.core.publisher.Mono;
 
 public interface UserRepositoryPort {
     Mono<User> findByEmail(String email);
-    Mono<Void> save(User user);
+    Mono<User> save(User user);
 }
